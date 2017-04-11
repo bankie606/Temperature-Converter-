4c418387f6a1ece6e1d7c7d7b5b093c9be61135f
bankie606 <frcrescioni@gmail.com>
2017-04-11 15:17:14 -0700

    Init Commit
    Hey there, this doesn’t really work well, but is a spot to start for
    sure. Maybe not the best place, but a place!

---
+++ temperatureconverter.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+
+<html>
+<head> 
+      <title>Temp Conversion Calculator</title>
+<!--COOL STYLING GOES HERE-->
+      <meta charset="utf-8">
+</head>    
+
+    <body>
+      <form name="tempConvert" method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+
+
+<h2>Enter value to convert</h2>
+    <input type="text" name="initValue" id="initValue" size="15">
+<select name="initialType" id="initialType" size="1">
+  <option disabled> Select a measurement type to start off with:</option>
+               <option value="celsius">Celsius</option>
+               <option value="fahrenheit">Fahrenheit</option>
+                <option value="kelvin">Kelvin</option>                  
+</select>
+          
+          
+          
+    <h3>Convert to:</h3>
+    <select name="convertType" id="convertType" size="1">
+               <option disabled> Select a measurement type to convert to:</option>
+               <option value="celsius">Celsius</option>
+               <option value="fahrenheit">Fahrenheit</option>
+                <option value="kelvin">Kelvin</option>
+        </select>
+
+<input type="submit" name="btnConvert" id="btnConvert" value="Convert">
+<input type="reset" name="btnReset" id="btnReset" value="Reset">
+
+
+
+
+</form>
+
+<?php
+
+function tempConvert($valueConvert, $convertType, $initValue, $initialType)
+{
+   if($convertType == "fahrenheit"){
+       $conversion = ((9/5) * $initValue) + (32);
+   }
+    else if ($convertType == "celsius"){
+       $conversion = ($initValue - 32) * (5/9);
+        
+   }
+    
+    else if ($convertType == "kelvin"){
+       $conversion = ($initValue - 32) * (5/9);
+        
+   }
+    
+    
+   return $conversion;
+}
+
+
+
+
+$valueConvert = $_POST['valueConvert'];
+$convertType = $_POST['convertType'];
+$conversion = tempConvert($valueConvert, $convertType);
+
+echo "The initial temperature was $valueConvert degrees. The new temperature is $conversion degrees.";
+
+?>
+          
+          </body>
+</html>
